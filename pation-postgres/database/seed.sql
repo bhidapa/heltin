9269a12f1598f9dde489bc5a9d8c7200c18b6fb5
Denis Badurina <badurinadenis@gmail.com>
2019-08-11 16:36:43 +0200

    refactor(seed): add client

--- pation-postgres/database/seed.sql
+++ pation-postgres/database/seed.sql
@@ -28,3 +28,30 @@ insert into public.user (id, email)
     (select id from new_private_user),
     'jane@doe.com'
   );
+
+-- client
+insert into public.client (
+  id,
+  "number",
+  first_name,
+  last_name,
+  date_of_birth,
+  telephone,
+  gender,
+  city,
+  address,
+  sent_by,
+  created_by
+) values (
+  '88bc02bf-000d-4120-8143-c0a095bb9192',
+  1,
+  'Johnatan',
+  'Doe',
+  '2001-02-03',
+  '123456789',
+  'MALE',
+  'London',
+  'Westminister',
+  'CLINIC',
+  '1d35ee19-e375-4a75-b855-76adda9d583e'
+);
