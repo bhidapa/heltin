13a21efc654f04edd5ab502d12aac94ad788dce3
Denis Badurina <badurinadenis@gmail.com>
2019-08-12 23:16:02 +0200

    feat(diagnostic/therapy): implement

--- heltin-postgres/database/public/multidisciplinary_diagnostic.sql
+++ heltin-postgres/database/public/multidisciplinary_diagnostic.sql
@@ -4,7 +4,61 @@ create table public.multidisciplinary_diagnostic (
   case_study_id         uuid not null references public.case_study(id) on delete cascade,
   case_study_session_id uuid references public.case_study_session(id) on delete cascade,
 
-  -- todo: add columns
+  "external" boolean not null,
+
+  description text,
+
+  created_at created_timestamptz not null,
+  updated_at updated_timestamptz not null
+
+  -- will be added after creating `public.multidisciplinary_therapy` since they both reference each other
+  -- multidisciplinary_therapy_id references public.multidisciplinary_therapy(id) on delete restrict
+);
+
+comment on column public.multidisciplinary_diagnostic."external" is 'If the diagnostics hasP been performed outside the institution in focus.';
+
+----
+
+create type public.multidisciplinary_diagnostic_report_type as enum (
+  'MULTIDISCIPLINARY_REPORT',
+  'PSYCHOTHERAPEUTIC_REPORT',
+  'PSYCHOLOGICAL_REPORT',
+  'PSYCHIATRIC_REPORT',
+  'NEUROLOGIC_REPORT',
+  'PEDIATRIC_REPORT',
+  'SOCIAL_WORKER_REPORT',
+  'DEFECTOLOGICAL_REPORT',
+  'T_O_V_A'
+);
+
+create table public.multidisciplinary_diagnostic_report_issued_to_type as enum (
+  'CLIENT',
+  'PARENT',
+  'KINDERGARTEN',
+  'SCHOOL',
+  'SOCIAL_WORK_CENTER',
+  'POLICE',
+  'HOSPITAL',
+  'PSYCHIATRIST',
+  'PEDIATRIST',
+  'COURT',
+  'MENTAL_HEALTH_CENTER',
+  'FOSTER_HOUSE'
+);
+
+create table public.multidisciplinary_diagnostic_report (
+  id uuid primary key default uuid_generate_v4(),
+
+  multidisciplinary_diagnostic_id uuid not null references public.multidisciplinary_diagnostic(id) on delete cascade,
+
+  report_type public.multidisciplinary_diagnostic_report_type not null,
+  report_url  text not null, -- medical report arriving with the diagnostic
+
+  issued_to public.multidisciplinary_diagnostic_report_issued_to_type[] check(array_length(issued_to, 1) is not null),
+  issued_at timestamptz,
+  check((issued_to is null) = (issued_at is null)), -- either both exist, or neither
+
+  description text,
 
   created_at created_timestamptz not null,
   updated_at updated_timestamptz not null
--- heltin-postgres/database/public/multidisciplinary_therapy.sql
+++ heltin-postgres/database/public/multidisciplinary_therapy.sql
@@ -1,11 +1,58 @@
+create type public.multidisciplinary_therapy_type as enum (
+  'INDIVIDUAL_INTEGRATIVE_THERAPY',
+  'INDIVIDUAL_GESTALT_THERAPY',
+  'INDIVIDUAL_COGNITIVE_BEHAVIOURAL_THERAPY',
+  'INDIVIDUAL_PSYCHOANALYTIC_THERAPY',
+  'COUPLE_THERAPY',
+  'FAMILY_THERAPY',
+  'GROUP_THERAPY',
+  'OCUPATIONAL_THERAPY',
+  'COUNSELING_THERAPY',
+  'PSYCHOEDUCATIONAL_THERAPY',
+  'ATTACHMENT_THERAPY',
+  'ATTACHMENT_BASIC_FAMILY_THERAPY',
+  'EYE_MOVEMENT_DESENSITIZATION_AND_REPROCESSING_THERAPY',
+  'TRAUMA_FOCUS_COGNITIVE_BEHAVIOURAL_THERAPY',
+  'FORENSICALLY_SENSITIVE_THERAPY',
+  'ANTIPSYCHOTIC_PSYCHOPHARMACOTHERAPY',
+  'ANTIDEPRESIVE_PSYCHOPHARMACOTHERAPY',
+  'ANXIOLYTIC_PSYCHOPHARMACOTHERAPY',
+  'MOOD_STABILIZER_PSYCHOPHARMACOTHERAPY',
+  'ANTIEPILEPTIC_PHARMACOTHERAPY',
+  'PHARMACOTHERAPY',
+  'PLAY_ATTENTION_THERAPY',
+  'DEFECTOLOGY_THERAPY',
+  'PHONETIC_THERAPY',
+  'PARENTAL_THERAPY',
+  -- external educational therapy
+  'SCHOOL_EDUCATION',
+  'KINDERGARTEN_EDUCATION',
+  'SOCIAL_CENTER_EDUCATION',
+  'COURT_EDUCATION'
+);
+
 create table public.multidisciplinary_therapy (
   id uuid primary key default uuid_generate_v4(),
 
   case_study_id         uuid not null references public.case_study(id) on delete cascade,
   case_study_session_id uuid references public.case_study_session(id) on delete cascade,
 
-  -- todo: add columns
+  multidisciplinary_diagnostic_id uuid references public.multidisciplinary_diagnostic(id) on delete restrict,
+
+  "external" boolean not null,
+
+  description text,
 
   created_at created_timestamptz not null,
   updated_at updated_timestamptz not null
 );
+
+comment on column public.multidisciplinary_therapy."external" is 'If the therapy has been performed outside the institution in focus.';
+
+----
+
+alter table public.multidisciplinary_diagnostic
+  add column multidisciplinary_therapy_id uuid,
+  add constraint multidisciplinary_diagnostic_multidisciplinary_therapy_id_fkey
+    foreign key (multidisciplinary_therapy_id)
+    references public.multidisciplinary_therapy(id) on delete restrict;
