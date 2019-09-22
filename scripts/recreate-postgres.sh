9eb1413cccc064ecef064c32ad74d320343e3a54
Denis Badurina <badurinadenis@gmail.com>
2019-09-22 14:39:26 +0200

    chore(postgres): recreate db on running postgres

---
+++ scripts/recreate-postgres.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd -P -- "$(dirname -- "${0}")" && pwd -P)
+
+# Load dotenv variables.
+source "${SCRIPT_DIR}"/dotenv.sh
+
+# Change dir to docker-compose config location.
+cd "${SCRIPT_DIR}/.." || exit 1
+
+docker-compose exec --user "${POSTGRES_USER}" postgres \
+  /docker-entrypoint-initdb.d/init.sh
