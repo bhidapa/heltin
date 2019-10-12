cc44e980a693fbb868b8f6510fbad6ebf96f82eb
Denis Badurina <badurinadenis@gmail.com>
2019-10-12 18:23:24 +0200

    feat(postgres): add trgm extension

--- heltin-postgres/database/extensions.sql
+++ heltin-postgres/database/extensions.sql
@@ -1,2 +1,3 @@
 create extension "uuid-ossp"; -- required for using uuids
 create extension "pgcrypto";  -- required for encrypting and evaluating passwords
+create extension "pg_trgm"; -- required for full-text searches
