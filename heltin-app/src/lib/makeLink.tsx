4ce6941f05912ec6432339c5977d0bb939a90d25
Denis Badurina <badurinadenis@gmail.com>
2019-10-13 19:18:35 +0200

    feat(makeLink): improvements

--- heltin-app/src/lib/makeLink.tsx
+++ heltin-app/src/lib/makeLink.tsx
@@ -4,14 +4,41 @@
  *
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, LinkProps } from 'react-router-dom';
+import _omit from 'lodash/fp/omit';
 
-export const makeLink = (linkProps: LinkProps) =>
-  function MadeLink({ children, ...rest }: React.Props<any>) {
-    return (
-      <Link {...rest} {...linkProps}>
-        {children}
-      </Link>
-    );
-  };
+export interface MakeLinkProps<P> extends LinkProps {
+  omit?: (keyof P)[];
+}
+
+const cache: {
+  [hash: string]: React.FC;
+} = {};
+
+export function makeLink<P>(props: MakeLinkProps<P>): React.FC<P> {
+  const hash = JSON.stringify(props);
+
+  if (!cache[hash]) {
+    const { omit, ...linkProps } = props;
+    const MadeLink: React.FC = ({ children, ...rest }) => {
+      // cache recycling
+      useEffect(
+        () => () => {
+          delete cache[hash];
+        },
+        [],
+      );
+
+      return (
+        <Link {...(omit ? _omit(omit, rest) : rest)} {...linkProps}>
+          {children}
+        </Link>
+      );
+    };
+
+    cache[hash] = MadeLink;
+  }
+
+  return cache[hash];
+}
