b84dd13cbfa2997dcfcfea7bdf95e1f4bc2a783c
enisdenjo <badurinadenis@gmail.com>
2020-09-23 01:18:53 +0200

    feat(graphql): begin with using sessions

--- cmd/graphql/src/index.ts
+++ cmd/graphql/src/index.ts
@@ -1,88 +1,106 @@
-const http = require("http");
+/**
+ *
+ * heltin GraphQL
+ *
+ */
+
+import express from "express";
+import session from "express-session";
+import { Pool } from "pg";
 import { postgraphile } from "postgraphile";
-const PgNonNullRelationsPlugin = require("@graphile-contrib/pg-non-null/relations");
+import connectPgSimple from "connect-pg-simple";
+
+// plugins
+import PgNonNullRelationsPlugin from "@graphile-contrib/pg-non-null/relations";
 import { PgIdToRowIdInflectorPlugin } from "./plugins/PgIdToRowIdInflectorPlugin";
-const PgBytea = require("./plugins/PgBytea");
+import PgBytea from "./plugins/PgBytea";
 
-const pgHost = process.env.POSTGRES_HOST;
-const pgUser = process.env.POSTGRES_USER;
-const pgPassword = process.env.POSTGRES_PASSWORD;
-const pgPort = process.env.POSTGRES_PORT;
-const pgDb = process.env.POSTGRES_DB;
-const pgSchemas = (process.env.POSTGRES_SCHEMAS || "").split(",");
-const watch = isTrue(process.env.WATCH);
-const graphqlRoute = process.env.GRAPHQL_ROUTE;
-const graphiqlRoute = process.env.GRAPHIQL_ROUTE;
-const jwtPgTypeIdentifier = process.env.JWT_POSTGRES_TYPE_IDENTIFIER;
-const jwtSecret = process.env.JWT_SECRET;
-const noAuth = isTrue(process.env.NO_AUTH);
-const port = process.env.PORT;
-
-start();
-
-function start() {
-  console.log();
-  console.info("Starting heltin GrahpQL...");
-  console.debug(
-    JSON.stringify(
-      {
-        pgHost,
-        pgUser,
-        pgPassword: pgPassword ? "<omitted>" : undefined,
-        pgPort,
-        pgDb,
-        pgSchemas,
-        watch,
-        graphqlRoute,
-        graphiqlRoute,
-        jwtPgTypeIdentifier,
-        jwtSecret: jwtSecret ? "<omitted>" : undefined,
-        noAuth,
-        port,
-      },
-      undefined,
-      "  "
-    )
-  );
-  console.log();
-
-  http
-    .createServer(
-      postgraphile(
-        `postgres://${pgUser}${
-          pgPassword ? `:${pgPassword}` : ""
-        }@${pgHost}:${pgPort}/${pgDb}`,
-        pgSchemas,
-        {
-          classicIds: true,
-          dynamicJson: true,
-          setofFunctionsContainNulls: false,
-          ignoreRBAC: false,
-          disableDefaultMutations: true,
-          disableQueryLog: false,
-          watchPg: watch,
-          graphileBuildOptions: {
-            pgStrictFunctions: true,
-          },
-          appendPlugins: [
-            PgNonNullRelationsPlugin,
-            PgIdToRowIdInflectorPlugin,
-            PgBytea,
-          ],
-          graphqlRoute,
-          graphiqlRoute,
-          graphiql: !!graphiqlRoute,
-          enhanceGraphiql: true,
-          jwtPgTypeIdentifier,
-          jwtSecret,
-          pgDefaultRole: noAuth ? "viewer" : "anonymous",
-          bodySizeLimit: "1GB",
-        }
-      )
-    )
-    .listen(port);
-}
+const config = {
+  pgHost: process.env.POSTGRES_HOST,
+  pgUser: process.env.POSTGRES_USER,
+  pgPassword: process.env.POSTGRES_PASSWORD,
+  pgPort: process.env.POSTGRES_PORT,
+  pgDb: process.env.POSTGRES_DB,
+  pgSchemas: (process.env.POSTGRES_SCHEMAS || "").split(","),
+  watch: isTrue(process.env.WATCH),
+  graphqlRoute: process.env.GRAPHQL_ROUTE,
+  graphiqlRoute: process.env.GRAPHIQL_ROUTE,
+  jwtPgTypeIdentifier: process.env.JWT_POSTGRES_TYPE_IDENTIFIER,
+  sessionSecret: process.env.SESSION_SECRET,
+  noAuth: isTrue(process.env.NO_AUTH),
+  port: process.env.PORT,
+};
 
 function isTrue(str: string | undefined): boolean {
   return ["t", "true", "1", "yes"].some((flag) => str === flag);
 }
+
+if (!config.sessionSecret) {
+  throw new Error(
+    "Session secret is required. Did you forget to set `SESSION_SECRET`?"
+  );
+}
+
+console.log();
+console.info("Starting heltin GrahpQL...");
+console.debug(
+  JSON.stringify(
+    {
+      ...config,
+      pgPassword: config.pgPassword ? "<omitted>" : undefined,
+      sessionSecret: config.sessionSecret ? "<omitted>" : undefined,
+    },
+    undefined,
+    "  "
+  )
+);
+console.log();
+
+const pgPool = new Pool({
+  connectionString: `postgres://${config.pgUser}${
+    config.pgPassword ? `:${config.pgPassword}` : ""
+  }@${config.pgHost}:${config.pgPort}/${config.pgDb}`,
+});
+
+const app = express();
+
+app.use(
+  session({
+    store: new (connectPgSimple(session))({
+      pool: pgPool,
+      tableName: "private.session",
+    }),
+    secret: config.sessionSecret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+  })
+);
+
+app.use(
+  postgraphile(pgPool, config.pgSchemas, {
+    classicIds: true,
+    dynamicJson: true,
+    setofFunctionsContainNulls: false,
+    ignoreRBAC: false,
+    disableDefaultMutations: true,
+    disableQueryLog: false,
+    watchPg: config.watch,
+    graphileBuildOptions: {
+      pgStrictFunctions: true,
+    },
+    appendPlugins: [
+      PgNonNullRelationsPlugin,
+      PgIdToRowIdInflectorPlugin,
+      PgBytea,
+    ],
+    graphqlRoute: config.graphqlRoute,
+    graphiql: !!config.graphiqlRoute,
+    graphiqlRoute: config.graphiqlRoute,
+    enhanceGraphiql: true,
+    pgDefaultRole: config.noAuth ? "viewer" : "anonymous",
+    bodySizeLimit: "1GB",
+  })
+);
+
+app.listen(config.port);
