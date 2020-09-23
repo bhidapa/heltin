04f309f19e4c608a44c2b223990f2dbe0c4f268a
Denis Badurina <denis@domonda.com>
2020-09-23 16:55:03 +0200

    chore(scripts): add postgres dump/restore scripts

---
+++ scripts/dump-postgres-data.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+CONTAINER=$(docker ps -q -f volume=heltin_postgres-data)
+DUMP_NAME=postgres_data_dump_$(date +%d-%m-%Y-%H-%M-%S).sql
+
+echo "Dumping from $CONTAINER..."
+docker exec "$CONTAINER" \
+  pg_dump -U postgres --data-only -d heltin -f "$DUMP_NAME"
+
+echo "Copying..."
+docker cp "$CONTAINER:$DUMP_NAME" .
+
+echo "Cleaning the container..."
+docker exec "$CONTAINER" rm "$DUMP_NAME"
+
+echo "Prepending the truncate statements..."
+docker exec "$CONTAINER" \
+  psql -U postgres --single-transaction -v ON_ERROR_STOP=1 \
+  -d heltin -c "COPY (
+    SELECT 'TRUNCATE ' || input_table_name || ' CASCADE;' AS truncate_query
+    FROM (SELECT table_schema || '.' || table_name AS input_table_name 
+      FROM information_schema.tables
+      WHERE table_schema NOT IN ('pg_catalog', 'information_schema') AND table_schema NOT LIKE 'pg_toast%') AS information
+  ) TO STDOUT;" | cat - "$DUMP_NAME" > "locked_$DUMP_NAME" && mv "locked_$DUMP_NAME" "$DUMP_NAME"
+
+echo "Prepending the session replication role..."
+echo -e "SET LOCAL session_replication_role TO replica;\n" | cat - "$DUMP_NAME" > "locked_$DUMP_NAME" && mv "locked_$DUMP_NAME" "$DUMP_NAME"
---
+++ scripts/restore-postgres-data.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+if [ -z "$1" ]; then
+  echo "Data dump file must be provided as the first argument"
+  exit 1
+fi
+
+CONTAINER=$(docker ps -q -f volume=heltin_postgres-data)
+DUMP_NAME="$1"
+
+echo "Copying dump to $CONTAINER..."
+docker cp "$DUMP_NAME" "$CONTAINER:$DUMP_NAME"
+
+# clean the container regardless of the outcome
+set +e
+echo "Restoring..."
+docker exec "$CONTAINER" \
+  psql -U postgres --single-transaction -v ON_ERROR_STOP=1 \
+  -d heltin -f "$DUMP_NAME"
+
+echo "Cleaning the container..."
+docker exec "$CONTAINER" rm "$DUMP_NAME"
