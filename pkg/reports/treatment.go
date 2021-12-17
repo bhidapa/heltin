8c8f87b2b5661f75f5d9a7e25e66d98078dd0ada
enisdenjo <badurinadenis@gmail.com>
2021-12-17 20:41:38 +0100

    feat(reports): treatment

--- pkg/reports/treatment.go
+++ pkg/reports/treatment.go
@@ -7,12 +7,13 @@ import (
 	"fmt"
 	"net/http"
 	"text/template"
+	"time"
 
 	"github.com/bhidapa/heltin/pkg/pdf"
 	"github.com/bhidapa/heltin/pkg/session"
-	"github.com/bhidapa/heltin/pkg/treatments"
 	"github.com/domonda/go-errs"
 	"github.com/domonda/go-sqldb/db"
+	"github.com/domonda/go-types/nullable"
 	"github.com/domonda/go-types/uu"
 	"github.com/gorilla/mux"
 	"github.com/ungerik/go-fs"
@@ -85,21 +86,70 @@ var zastitaZdravljaPng []byte
 func forTreatmentInPDF(ctx context.Context, treatmentID uu.ID) (pdfFile *fs.MemFile, err error) {
 	defer errs.WrapWithFuncParams(&err, ctx, treatmentID)
 
-	var treatment treatments.Treatment
-	err = db.Conn(ctx).QueryRow("select * from public.case_study_treatment where id = $1", treatmentID).
+	treatment := struct {
+		// therapist shouldn't be null, but the user that created
+		// the case study is not a mental health professional
+		TherapistName      nullable.NonEmptyString `db:"therapist_name"`
+		TherapistEmail     nullable.NonEmptyString `db:"therapist_email"`
+		TherapistTelephone nullable.NonEmptyString `db:"therapist_telephone"`
+		ClientName         string                  `db:"client_name"`
+		ClientEmail        nullable.NonEmptyString `db:"client_email"`
+		ClientTelephone    string                  `db:"client_telephone"`
+		Title              string                  `db:"title"`
+		Description        string                  `db:"description"`
+		StartedAtTime      time.Time               `db:"started_at"`
+		StartedAt          string                  `db:"-"`
+		EndedAtTime        time.Time               `db:"ended_at"`
+		EndedAt            string                  `db:"-"`
+	}{}
+	err = db.Conn(ctx).QueryRow(`
+		-- TODO: support group case studies
+		select
+			public.mental_health_professional_full_name(mental_health_professional) as therapist_name,
+			mental_health_professional.email as therapist_email,
+			mental_health_professional.telephone as therapist_telephone,
+
+			public.client_full_name(client) as client_name,
+			client.email as client_email,
+			client.telephone as client_telephone,
+
+			case_study_treatment.title as title,
+			case_study_treatment.description as description,
+
+			case_study_treatment.started_at as started_at,
+			case_study_treatment.ended_at as ended_at
+		from public.case_study_treatment
+			inner join (public.case_study
+				inner join public.client on client.id = case_study.client_id)
+			on case_study.id = case_study_treatment.case_study_id
+			left join public.mental_health_professional
+			on mental_health_professional.user_id = case_study_treatment.created_by
+		where case_study_treatment.id = $1`, treatmentID).
 		ScanStruct(&treatment)
 	if err != nil {
 		return nil, err
 	}
 
-	data := map[string]string{
+	// TODO: use timezone from requester
+	loc, err := time.LoadLocation("Europe/Sarajevo")
+	if err != nil {
+		return nil, err
+	}
+	treatment.StartedAt = treatment.StartedAtTime.In(loc).Format("02.01.2006 15:04")
+	treatment.EndedAt = treatment.EndedAtTime.In(loc).Format("02.01.2006 15:04")
+
+	fileCreatedBy, err := session.UserFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	data := map[string]interface{}{
 		"Logo":                      pdf.ToBase64Encoding(bhidapaLogo),
 		"ZastitaZdravlja":           pdf.ToBase64Encoding(zastitaZdravljaPng),
 		"DjecijaDusaTrebaDaSeSlusa": pdf.ToBase64Encoding(djecijaDusaTrebaDaSeSlusaPng),
-
-		"Title":       treatment.Title,
-		"Description": treatment.Description.StringOr(""),
-		// TODO-db-121121 add rest
+		"ID":                        treatmentID,
+		"Treatment":                 treatment,
+		"FileCreatedBy":             fileCreatedBy,
+		"FileCreatedAt":             time.Now().UTC(),
 	}
 
 	headerHTML, err := render(treatmentHeaderHtml, data)
