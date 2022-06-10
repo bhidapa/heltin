7852f2dfb44887a7edc1c8c62b04b786e33330b9
Denis Badurina <denis@domonda.com>
2022-06-10 13:52:43 +0200

    feat(ClientCaseStudies): display "no entries" when empty

--- web/src/modules/Client/ClientCaseStudies.tsx
+++ web/src/modules/Client/ClientCaseStudies.tsx
@@ -77,90 +77,98 @@ export const ClientCaseStudies: React.FC<ClientCaseStudiesProps> = (props) => {
         </div>
       </div>
 
-      {client.caseStudies.nodes.map((caseStudy) => (
-        <div key={caseStudy.id} className="card mx-0">
-          <div className="row mb-20 align-items-center">
-            <div className="col">
-              <h4 className="card-title m-0">
-                <Link to={`case-studies/${caseStudy.rowId}`} search>
-                  {caseStudy.title}
-                </Link>
-              </h4>
-            </div>
-
-            {caseStudy.conclusion ? (
-              <div className="col text-right">
-                <Tooltip content={<FormattedMessage id={caseStudy.conclusion.type} />}>
-                  <i className="fa-solid fa-clipboard-check text-primary font-size-20"></i>
-                </Tooltip>
-              </div>
-            ) : (
+      {client.caseStudies.nodes.length === 0 ? (
+        <div className="row">
+          <p className="text-muted">
+            <FormattedMessage id="NO_ENTRIES" />
+          </p>
+        </div>
+      ) : (
+        client.caseStudies.nodes.map((caseStudy) => (
+          <div key={caseStudy.id} className="card mx-0">
+            <div className="row mb-20 align-items-center">
               <div className="col">
-                {/* flex-wrap-reverse so that "new treatment" is on above "conclude" when stacked */}
-                <div className="row flex-wrap-reverse justify-content-end align-items-center">
-                  <div className="col-auto mr-0 mr-md-20">
-                    <Link
-                      to={`case-studies/${caseStudy.rowId}/conclusion/create`}
-                      search
-                      className="btn btn-secondary"
-                    >
-                      <i className="fa-solid fa-clipboard-check"></i>
-                      &nbsp;
-                      <FormattedMessage id="CONCLUDE" />
-                    </Link>
-                  </div>
+                <h4 className="card-title m-0">
+                  <Link to={`case-studies/${caseStudy.rowId}`} search>
+                    {caseStudy.title}
+                  </Link>
+                </h4>
+              </div>
 
-                  <div className="form-v-spacer hidden-md-and-up" />
-
-                  <div className="col-auto">
-                    <div className="btn-group" role="group">
-                      <Dropdown
-                        content={
-                          <>
-                            <h6 className="dropdown-header">
-                              <FormattedMessage id="AVAILABLE_FORMS" />
-                            </h6>
-                            {forms.map((node) => (
-                              <Link
-                                key={node.rowId}
-                                to={`case-studies/${caseStudy.rowId}/forms/fill/${node.rowId}`}
-                                search
-                                className="dropdown-item"
-                              >
-                                {node.name}
-                              </Link>
-                            ))}
-                          </>
-                        }
-                      >
-                        {(trigger) => (
-                          <button type="button" className="btn" onClick={trigger}>
-                            <i className="fa-solid fa-file-pen"></i>
-                            &nbsp;
-                            <FormattedMessage id="FILL_FORM" />
-                          </button>
-                        )}
-                      </Dropdown>
-
+              {caseStudy.conclusion ? (
+                <div className="col text-right">
+                  <Tooltip content={<FormattedMessage id={caseStudy.conclusion.type} />}>
+                    <i className="fa-solid fa-clipboard-check text-primary font-size-20"></i>
+                  </Tooltip>
+                </div>
+              ) : (
+                <div className="col">
+                  {/* flex-wrap-reverse so that "new treatment" is on above "conclude" when stacked */}
+                  <div className="row flex-wrap-reverse justify-content-end align-items-center">
+                    <div className="col-auto mr-0 mr-md-20">
                       <Link
-                        to={`case-studies/${caseStudy.rowId}/treatments/create`}
+                        to={`case-studies/${caseStudy.rowId}/conclusion/create`}
                         search
-                        className="btn btn-primary"
+                        className="btn btn-secondary"
                       >
-                        <i className="fa-solid fa-hand-holding-medical"></i>
+                        <i className="fa-solid fa-clipboard-check"></i>
                         &nbsp;
-                        <FormattedMessage id="NEW_CASE_STUDY_TREATMENT" />
+                        <FormattedMessage id="CONCLUDE" />
                       </Link>
+                    </div>
+
+                    <div className="form-v-spacer hidden-md-and-up" />
+
+                    <div className="col-auto">
+                      <div className="btn-group" role="group">
+                        <Dropdown
+                          content={
+                            <>
+                              <h6 className="dropdown-header">
+                                <FormattedMessage id="AVAILABLE_FORMS" />
+                              </h6>
+                              {forms.map((node) => (
+                                <Link
+                                  key={node.rowId}
+                                  to={`case-studies/${caseStudy.rowId}/forms/fill/${node.rowId}`}
+                                  search
+                                  className="dropdown-item"
+                                >
+                                  {node.name}
+                                </Link>
+                              ))}
+                            </>
+                          }
+                        >
+                          {(trigger) => (
+                            <button type="button" className="btn" onClick={trigger}>
+                              <i className="fa-solid fa-file-pen"></i>
+                              &nbsp;
+                              <FormattedMessage id="FILL_FORM" />
+                            </button>
+                          )}
+                        </Dropdown>
+
+                        <Link
+                          to={`case-studies/${caseStudy.rowId}/treatments/create`}
+                          search
+                          className="btn btn-primary"
+                        >
+                          <i className="fa-solid fa-hand-holding-medical"></i>
+                          &nbsp;
+                          <FormattedMessage id="NEW_CASE_STUDY_TREATMENT" />
+                        </Link>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            )}
-          </div>
+              )}
+            </div>
 
-          <EventsTable events={caseStudy.sortedEvents.nodes} />
-        </div>
-      ))}
+            <EventsTable events={caseStudy.sortedEvents.nodes} />
+          </div>
+        ))
+      )}
     </div>
   );
 };
