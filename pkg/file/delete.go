2fb695c04a8bc7757da75ac53ea90f65ba2e9e05
enisdenjo <badurinadenis@gmail.com>
2022-10-17 22:30:40 +0200

    WIP

--- pkg/file/delete.go
+++ pkg/file/delete.go
@@ -25,47 +25,68 @@ func Delete(ctx context.Context, id uu.ID) (err error) {
 			return session.ErrForbidden
 		}
 
-		fsfile, err := FileFromID(ctx, id)
-		if err != nil {
-			return err
-		}
-
-		toRemove := fsfile.Dir()
-		for {
-			parentDir := toRemove.Dir()
-
-			parentDirFiles, err := parentDir.ListDirMaxContext(ctx, 2)
-			if err != nil {
-				return err
-			}
-			if len(parentDirFiles) > 1 {
-				// if the parent directory contains more files
-				// we'll remove the current directory
-				break
-			}
-			if config.Dir.Path() == parentDir.Path() {
-				// if the parent directory is the root files directory
-				// we'll remove the current directory
-				break
-			}
-
-			// continue going up until dir contains multiple files
-			toRemove = parentDir
-		}
-
-		// make sure there is ONLY ONE FILE within the directory to remove (ListDirRecursiveContext lists only files, skips directories)
-		fileFound := false
-		err = toRemove.ListDirRecursiveContext(ctx, func(f fs.File) error {
-			if fileFound {
-				return errs.New("multiple files exist under the dir to be removed")
-			}
-			fileFound = true
-			return nil
-		})
-		if err != nil {
-			return err
-		}
-
-		return toRemove.RemoveRecursiveContext(ctx)
+		return deleteFromFS(ctx, id)
 	})
+}
+
+func ForceDelete(ctx context.Context, id uu.ID) (err error) {
+	defer errs.WrapWithFuncParams(&err, ctx, id)
+
+	return db.Transaction(ctx, func(ctx context.Context) error {
+		var deleted bool
+		err := db.Conn(ctx).QueryRow("delete from public.file where id = $1 returning true", id).Scan(&deleted)
+		if db.ReplaceErrNoRows(err, nil) != nil {
+			return err
+		}
+
+		err = deleteFromFS(ctx, id)
+		return ReplaceErrNotExists(err, nil)
+	})
+}
+
+func deleteFromFS(ctx context.Context, id uu.ID) (err error) {
+	defer errs.WrapWithFuncParams(&err, ctx, id)
+
+	fsfile, err := FileFromID(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	toRemove := fsfile.Dir()
+	for {
+		parentDir := toRemove.Dir()
+
+		parentDirFiles, err := parentDir.ListDirMaxContext(ctx, 2)
+		if err != nil {
+			return err
+		}
+		if len(parentDirFiles) > 1 {
+			// if the parent directory contains more files
+			// we'll remove the current directory
+			break
+		}
+		if config.Dir.Path() == parentDir.Path() {
+			// if the parent directory is the root files directory
+			// we'll remove the current directory
+			break
+		}
+
+		// continue going up until dir contains multiple files
+		toRemove = parentDir
+	}
+
+	// make sure there is ONLY ONE FILE within the directory to remove (ListDirRecursiveContext lists only files, skips directories)
+	fileFound := false
+	err = toRemove.ListDirRecursiveContext(ctx, func(f fs.File) error {
+		if fileFound {
+			return errs.New("multiple files exist under the dir to be removed")
+		}
+		fileFound = true
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return toRemove.RemoveRecursiveContext(ctx)
 }
--- pkg/file/errors.go
+++ pkg/file/errors.go
@@ -1,5 +1,16 @@
 package file
 
-import "github.com/domonda/go-errs"
+import (
+	"errors"
+
+	"github.com/domonda/go-errs"
+)
 
 const ErrNotExist = errs.Sentinel("file does not exist")
+
+func ReplaceErrNotExists(err error, replacement error) error {
+	if errors.Is(err, ErrNotExist) {
+		return replacement
+	}
+	return err
+}
