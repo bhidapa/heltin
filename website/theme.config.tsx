3bc153cb3094f4f8a0f396517fab3d43f2b85162
enisdenjo <denis@denelop.com>
2023-11-23 17:11:35 +0100

    use heltin from theguild components

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,19 +1,6 @@
-import React from 'react';
-import { defineConfig, useTheme } from '@theguild/components';
-
-const siteName = 'heltin';
+import { defineConfig } from '@theguild/components';
 
 export default defineConfig({
+  siteName: 'HELTIN',
   docsRepositoryBase: 'https://github.com/bhidapa/heltin',
-  logo: (
-    <div>
-      <h1 className="md:text-md text-sm font-medium">{siteName}</h1>
-      <h2 className="hidden text-xs sm:block">Mental healthcare registry</h2>
-    </div>
-  ),
-  main({ children }) {
-    useTheme();
-    return <>{children}</>;
-  },
-  siteName,
 });
